d2a280d07d925d5959f26d709fb5e6659ecac99a
SreeCharanManne <51262836+SreeCharanManne@users.noreply.github.com>
2020-03-27 19:40:37 +0530

    gfg

--- Minimum indexed character.java
+++ Minimum indexed character.java
@@ -2,19 +2,15 @@ class PrintString{
     
     // Function to find the character in patt which is present in str at min index
     public static StringBuffer printMinIndexChar(String str, String patt){
-        
-        // Your code here
-        
-        // you don't need to print anything
+
        StringBuffer sb=new StringBuffer("$");
-       int[] a = new int[1000];
+       int[] a = new int[26];
        for(char c:patt.toCharArray())
-       a[c]=1;
+       a[c-'a']=1;
        for(char c:str.toCharArray())
-       if(a[c] != 0){
+       if(a[c-'a'] != 0){
        sb =new StringBuffer(c+"");
        break;}
-// you don't need to print anything
 return sb;
         
     
