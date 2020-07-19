024c05f45c3ff0f2e1bca4ad59a93094ce00695d
SreeCharanManne <51262836+SreeCharanManne@users.noreply.github.com>
2020-07-19 11:27:12 +0530

    leetcode

---
+++ Number of Nodes in the Sub-Tree With the Same Label.java
@@ -0,0 +1,71 @@
+ 
+import java.util.*;
+class Solution {
+    class Node
+	{
+		int index;
+		char value;
+		List<Node> child = new ArrayList<>();
+
+		public Node(int index, char value)
+		{
+			this.index = index;
+			this.value = value;
+		}
+	}
+
+	public int[] countSubTrees(int n, int[][] edges, String labels)
+	{
+		int[] answer = new int[n];
+		Map<Integer, Node> nodes = new HashMap<>();
+		for (int[] edge : edges)
+		{
+			Node node = nodes.get(edge[0]);
+			if (node == null)
+			{
+				node = new Node(edge[0], labels.charAt(edge[0]));
+				nodes.put(edge[0], node);
+			}
+
+			Node child = nodes.get(edge[1]);
+			if (child == null)
+			{
+				child = new Node(edge[1], labels.charAt(edge[1]));
+				nodes.put(edge[1], child);
+			}
+
+			node.child.add(child);
+			child.child.add(node);
+		}
+
+		dfs(nodes.get(0), answer, new HashSet<>());
+
+		return answer;
+	}
+
+	private int[] dfs(Node node, int[] answer, Set<Integer> visited)
+	{
+		if (node == null || !visited.add(node.index))
+		{
+			return new int[0];
+		}
+
+		int[] memo = new int[26];
+		memo[node.value - 'a']++;
+
+		for (Node child : node.child)
+		{
+			int[] count = dfs(child, answer, visited);
+			for (int i = 0; i < count.length; i++)
+			{
+				memo[i] += count[i];
+			}
+		}
+
+		answer[node.index] = memo[node.value - 'a'];
+		return memo;
+	}
+}
+ 
+
+    
